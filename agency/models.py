ddd036353a16ab16c5ce1a5dd40a4e592cc3a48f
TracyOgutu <tracyogutu@gmail.com>
2021-08-16 00:49:26 +0300

    make migrations

--- agency/models.py
+++ agency/models.py
@@ -79,17 +79,7 @@ class House(models.Model):
     def __str__(self):
         return self.housename
 
-class House(models.Model):
-    houseName = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='houses')
-    location = models.CharField(max_length=100)
-    rooms = models.IntegerField()
-    price = models.IntegerField()
-    published = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.houseName
+
 
 
 
