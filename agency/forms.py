e1ed6371a2e068a8d6e65553879af88e929dc359
TracyOgutu <tracyogutu@gmail.com>
2021-08-16 00:02:05 +0300

    add profile form

--- agency/forms.py
+++ agency/forms.py
@@ -2,14 +2,19 @@ from django import forms
 from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import TextInput, EmailInput
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
+    
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('firstname', 'lastname','birth_date','interestedin',)
 
 
 
