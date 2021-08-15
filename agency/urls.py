c4ebaba71b1749cf5c924dcf282eaf9ac10534e5
TracyOgutu <tracyogutu@gmail.com>
2021-08-16 00:02:36 +0300

    add password reset urls

--- agency/urls.py
+++ agency/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
@@ -11,5 +13,13 @@ urlpatterns=[
     url(r'^logout/$', auth_views.LogoutView.as_view(next_page='login') , name='logout'),
     url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.activate, name='activate'),
-   
+    url(r'^password_reset/$',auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name='password_reset_complete'),
+    url(r'^update/profile$', views.update_profile, name='update_profile'),
+    url(r'displayprofile/(\d+)',views.display_profile,name='displayprofile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
