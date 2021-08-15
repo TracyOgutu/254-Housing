44ed0def31c8a650ad61d500c34a9d4008f68a16
TracyOgutu <tracyogutu@gmail.com>
2021-08-15 23:58:01 +0300

    add housing model to admin

--- agency/admin.py
+++ agency/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from .models import House
 
 # Register your models here.
 
+admin.site.register(House)
+admin.site.site_header  =  "+254 Housing admin"  
+admin.site.site_title  =  "+254 Housing admin site"
+admin.site.index_title  =  "+254 Housing Admin"
 
 
 
